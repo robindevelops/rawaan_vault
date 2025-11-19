5bdca323126881152eb6b799b16963deb3cb67a7
Alyan Shahid <alyanshahidsdk@gmail.com>
2025-11-19 21:21:55 +0500

    first commit

---
+++ lib/presentation/widgets/buttons/buttons_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ButtonsScreen extends StatelessWidget {
+  const ButtonsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
